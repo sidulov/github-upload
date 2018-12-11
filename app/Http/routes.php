d12e66fc01a2fc673bb125523811a3c610ace5d0
sidulov <45783981+sidulov@users.noreply.github.com>
2018-12-11 13:24:40 +0400

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,19 +8,4 @@ Route::get('/tas', 'TaskController@index');
 Route::post('/task', 'TaskController@store');
 Route::delete('/task/{task}', 'TaskController@destroy');
 
-/**
- * Destroy the given task.
- *
- * @param  Request  $request
- * @param  Task  $task
- * @return Response
- */
-public function destroy(Request $request, Task $task)
-{
-    $this->authorize('destroy', $task);
-
-    $task->delete();
-
-    return redirect('/tas');
-}
 });
